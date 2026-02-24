0f9fc0a2fee6c69d9d3a411831bbeb7b29c83a4b
RethLopes <RethLopes@users.noreply.github.com>
2026-02-24 17:54:13 +0000

    deploy: 455798fc061f542b035866d159c080a4000a2910

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1634+g3364af43e',
+    VERSION: '0.0.post1635+g455798fc0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
